fe41f73a11cef09065462ba882cb19d7600e059d
BelminMusic <150080851+BelminMusic@users.noreply.github.com>
2024-04-18 15:49:08 +0200

    cleaned up

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 const addButton = document.getElementById('addTask');
 const taskInput = document.getElementById('taskInput');
 const taskList = document.getElementById('taskList');
@@ -6,62 +5,58 @@ const taskList = document.getElementById('taskList');
 loadTasks();
 
 function addTask() {
-
     const task = taskInput.value.trim();
 
     if (task) {
-
-        createTaskElement(task)
-
+        createTaskElement(task);
         taskInput.value = "";
-
-        saveTask()
-
+        saveTask();
     } else {
-        alert('Enter a task!')
+        alert('Enter a task!');
     }
-
 }
 
 addButton.addEventListener('click', addTask);
 
 function createTaskElement(task) {
-
     const listItem = document.createElement('li');
-
     listItem.textContent = task;
 
     const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'delete';
+    deleteButton.textContent = '✘';
     deleteButton.className = 'deleteTask';
 
+    const checkButton = document.createElement('button');
+    checkButton.textContent = '💯';
+    checkButton.className = 'checkTask';
+
+    listItem.appendChild(checkButton);
     listItem.appendChild(deleteButton);
     taskList.appendChild(listItem);
 
     deleteButton.addEventListener('click', function() {
         taskList.removeChild(listItem);
-        saveTasks();
+        saveTask();
     });
 
+    checkButton.addEventListener('click', function() {
+        listItem.style.textDecoration = 'line-through';
+        saveTask();
+    });
 }
 
-
 function saveTask() {
-
     let tasks = [];
     taskList.querySelectorAll('li').forEach(function(item) {
         tasks.push(item.textContent.trim());
     });
 
     localStorage.setItem('tasks', JSON.stringify(tasks));
-
 }
 
-
 function loadTasks() {
-
     const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
-
-    tasks.forEach(createTaskElement);
-
+    tasks.forEach(function(task) {
+        createTaskElement(task);
+    });
 }
