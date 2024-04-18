72a8cb22dc1567029b11f09f3ec1ca5011a67c39
BelminMusic <150080851+BelminMusic@users.noreply.github.com>
2024-04-18 14:41:59 +0200

    commit

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 
 const addButton = document.getElementById('addTask');
 const taskInput = document.getElementById('taskInput');
-const tasklist = document.getElementById('taskList');
+const taskList = document.getElementById('taskList');
+
+loadTasks();
 
 function addTask() {
 
@@ -12,6 +14,11 @@ function addTask() {
         createTaskElement(task)
 
         taskInput.value = "";
+
+        saveTask()
+
+    } else {
+        alert('Enter a task!')
     }
 
 }
@@ -19,5 +26,42 @@ function addTask() {
 addButton.addEventListener('click', addTask);
 
 function createTaskElement(task) {
+
+    const listItem = document.createElement('li');
+
+    listItem.textContent = task;
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'delete';
+    deleteButton.className = 'deleteTask';
+
+    listItem.appendChild(deleteButton);
+    taskList.appendChild(listItem);
+
+    deleteButton.addEventListener('click', function() {
+        taskList.removeChild(listItem);
+        saveTasks();
+    });
+
+}
+
+
+function saveTask() {
+
+    let tasks = [];
+    taskList.querySelectorAll('li').forEach(function(item) {
+        tasks.push(item.textContent.trim());
+    });
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+
+}
+
+
+function loadTasks() {
+
+    const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+    tasks.forEach(createTaskElement);
 
 }
